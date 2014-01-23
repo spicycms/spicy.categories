13f86bdf3e34ad4b1eab2ee88aea8180f9a82e67
Stas Shtin <antisvin@gmail.com>
2014-01-23 17:19:44 +0400

    No more spicy.common

--- src/spicy/categories/admin.py
+++ src/spicy/categories/admin.py
@@ -7,9 +7,8 @@ from django.shortcuts import get_object_or_404
 from django.utils.translation import ugettext_lazy as _
 from spicy.core.admin.conf import AdminAppBase, AdminLink, Perms
 from spicy.core.profile.decorators import is_staff
-from spicy.core.siteskin.common import NavigationFilter
 from spicy.core.siteskin.decorators import ajax_request, render_to
-from spicy.utils import get_custom_model_class
+from spicy.utils import get_custom_model_class, NavigationFilter
 from spicy.utils.permissions import *
 from . import defaults, forms
 
